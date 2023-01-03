94d5e0de266cfc01774f847dce2167a13713d7b0
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2023-01-03 22:45:16 +0100

    clean comment

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,12 +1,5 @@
 const multer = require("multer")
 
-// const MIME_TYPES = {
-//     "image/jpg": "jpg",
-//     "image/jpeg": "jpg",
-//     "image/png": "png",
-//     "image/svg": "svg",
-// };
-
 const storageEngine = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "public/pictures")
